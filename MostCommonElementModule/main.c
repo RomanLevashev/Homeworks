baafb27e1b0f9403ef6b9cf05f15100cc7c52235
RomanLevashev <rolevashev@gmail.com>
2025-02-10 21:15:38 +0300

    Tabs to spaces

--- MostCommonElementModule/main.c
+++ MostCommonElementModule/main.c
@@ -5,22 +5,22 @@
 #include "test.h"
 
 int main(void) {
-	if (runTests()) {
-		puts("All test passed successfully");
-	}
-	else {
-		return 1;
-	}
-	int n = 10;
-	FILE* inputData = fopen("file.txt", "r");
-	int* array = calloc(n, sizeof(int));
-	int i = 0;
+    if (runTests()) {
+        puts("All test passed successfully");
+    }
+    else {
+        return 1;
+    }
+    int n = 10;
+    FILE* inputData = fopen("file.txt", "r");
+    int* array = calloc(n, sizeof(int));
+    int i = 0;
 
-	while (fscanf(inputData, "%d ", &array[i]) == 1) {
-		i++;
-	}
+    while (fscanf(inputData, "%d ", &array[i]) == 1) {
+        i++;
+    }
 
-	printf("%d", mostCommonElement(array, i + 1));
-	free(array);
-	return 0;
+    printf("%d", mostCommonElement(array, i + 1));
+    free(array);
+    return 0;
 }
--- MostCommonElementModule/mostCommonElement.c
+++ MostCommonElementModule/mostCommonElement.c
@@ -2,24 +2,24 @@
 #include "quickSort.h"
 
 int mostCommonElement(int array[], int length) {
-	quickSort(array, length);
-	int count = 1;
-	int max_count = 1;
-	int commonElement = array[0];
-	int last = array[0];
+    quickSort(array, length);
+    int count = 1;
+    int max_count = 1;
+    int commonElement = array[0];
+    int last = array[0];
 
-	for (int i = 1; i < length; ++i) {
-		if (array[i] == last) {
-			count++;
-		}
-		if (array[i] != last || i == length - 1) {
-			if (count > max_count) {
-				commonElement = last;
-				max_count = count;
-			}
-			last = array[i];
-			count = 1;
-		}
-	}
-	return commonElement;
+    for (int i = 1; i < length; ++i) {
+        if (array[i] == last) {
+            count++;
+        }
+        if (array[i] != last || i == length - 1) {
+            if (count > max_count) {
+                commonElement = last;
+                max_count = count;
+            }
+            last = array[i];
+            count = 1;
+        }
+    }
+    return commonElement;
 }
